bd7424e7caba852440cc2318f379d4e5c2e95da1
HIPPOWUYUFAN <hippo@hippodeMacBook-Pro.local>
2020-04-27 00:06:59 +0800

    add webpack  optimization

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 const babelConfig = {
     presets: ["@babel/preset-react", "@babel/preset-env"],
     plugins: [
+        "@babel/plugin-transform-runtime",
         ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ]
+        ['import', {
+            libraryName: '@material-ui',
+            style: true
+        }]
+    ],
 }
 
 module.exports = babelConfig;
--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const devMode = process.env.NODE_ENV !== 'production'
+console.log(devMode)
 const htmlWebPackPlugin = require('html-webpack-plugin') // 导入 在内存中自动生成index页面的插件 ，自动打包好的js文件追加入index中
 const htmlPlugin = new htmlWebPackPlugin({
     template: path.join(__dirname, '../index.html'), // 源文件
     filename: 'index.html',  // 生成内存中首页的名称
     inject: true,
+    minify:{//生产期间使用，直接设置为true，开发时设置为false
+        // removeAttributeQuotes:true,//删除双引号
+        removeComments:true,//删除注释
+        collapseWhitespace:false,//压缩代码
+        removeStyleLinkTypeAttributes:false,
+        removeScriptTypeAttributes:false
+    }
 })
+
+
 const copyWebpackPlugin = require("copy-webpack-plugin");
 const copyPlugin = new copyWebpackPlugin([{
     from: path.resolve(__dirname, "../favicon.ico"),
@@ -12,15 +23,18 @@ const copyPlugin = new copyWebpackPlugin([{
     ignore: ['.*']
 }])
 
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production'
 const miniCssExtractPlugin = new MiniCssExtractPlugin({
     // 这里的配置和webpackOptions.output中的配置相似
     // 即可以通过在名字前加路径，来决定打包后的文件存在的路径
-    filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
-    chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css',
+    filename: devMode ? 'static/css/[name].css' : 'static/css/[name].[hash].css',
+    chunkFilename: devMode ? 'static/css/[id].css' : 'static/css/[id].[hash].css',
 })
 
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
 
     /*入口*/
@@ -29,14 +43,37 @@ module.exports = {
     plugins: [
         htmlPlugin,
         miniCssExtractPlugin,
-        copyPlugin
+        copyPlugin,
+        // new BundleAnalyzerPlugin({ analyzerPort: 8080 })
     ],
     /*输出到dist目录，输出文件名字为bundle.js*/
     output: {
         path: path.join(__dirname, '../dist'),
-        filename: 'bundle.js'
+        filename:'static/js/[name].bundle.js',
     },
-
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({//压缩js
+                extractComments: false, // 是否将注释单独提出来一个文件
+                cache: true,
+                parallel: true,// 开启并行压缩，充分利用cpu
+                sourceMap: devMode,
+            }),
+            new OptimizeCSSAssetsPlugin()//压缩css
+        ],
+        concatenateModules: true,
+        splitChunks: {
+            cacheGroups: {
+                vendors: {//node_modules里的代码
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: "initial",
+                    name: 'vendors',
+                    priority: 10,
+                    enforce: true //强制生成
+                },
+            }
+        }
+    },
     module: {
         rules: [
             {
@@ -56,7 +93,7 @@ module.exports = {
                             // 如果打包后，background属性中的图片显示不出来，请检查publicPath的配置是否有误
                             publicPath: './',
                             // publicPath: devMode ? './' : '../',   // 根据不同环境指定不同的publicPath
-                            hmr: devMode, // 仅dev环境启用HMR功能
+                            hmr: 'development', // 仅dev环境启用HMR功能
                         },
                     },
                     'css-loader'
@@ -69,8 +106,8 @@ module.exports = {
                     loader: 'url-loader',
                     options: { // 这里的options选项参数可以定义多大的图片转换为base64
                         fallback: "file-loader",
-                        limit: 10 * 1024, // 表示小于10kb的图片转为base64,大于10kb的是路径
-                        outputPath: 'images', //定义输出的图片文件夹
+                        limit: 3 * 1024, // 表示小于10kb的图片转为base64,大于10kb的是路径
+                        outputPath:'static/img/', //定义输出的图片文件夹
                         name: '[name].[contenthash:8].[ext]'
                     }
                 }],
@@ -84,8 +121,8 @@ module.exports = {
                         options: {
                             name: "[name]-[hash:5].min.[ext]",
                             limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
-                            publicPath: "fonts/",
-                            outputPath: "fonts/"
+                            publicPath: "static/font/",
+                            outputPath: "static/font/"
                         }
                     }
                 ],
@@ -114,6 +151,7 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     resolve: {
+        modules:[path.resolve(__dirname, "src"), "node_modules"],
         extensions: [".js", ".jsx", ".json"],
         alias: {
             '@pages': path.join(__dirname, '../src/pages/'),
@@ -127,3 +165,5 @@ module.exports = {
     },
 
 };
+
+console.log(process.env.NODE_ENV)
